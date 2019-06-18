ef08ab73df03dae35e82a96fb4937b32386d1ba0
Drew11 <drew.naumenko@gmail.com>
2019-06-18 09:59:46 +0300

    Updates

---
+++ precache-manifest.2124ad009b96657b976b0760416b3297.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd09100c5785741dcd04ced5baf12358",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f2cfc580feb7a86c60d1",
+    "url": "/tic-tac-toe/static/css/main.f2187bdf.chunk.css"
+  },
+  {
+    "revision": "c63d85398e1a472bd830",
+    "url": "/tic-tac-toe/static/js/2.72912114.chunk.js"
+  },
+  {
+    "revision": "f2cfc580feb7a86c60d1",
+    "url": "/tic-tac-toe/static/js/main.a871bf8f.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
